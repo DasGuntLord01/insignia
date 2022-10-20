241d90cdfc00a15d7f84205b83a784ceb538b165
jwilliams <jwilliams@bar-tech.com.au>
2022-10-20 13:04:33 +1000

    and save to the server

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
+/*
+Just a simple backend node server
+To handle serving the index and handle the save request
+ */
+
 let http = require('http');
 let fs = require('fs');
 
@@ -5,13 +12,13 @@ let server = http.createServer( (req,res) => {
 
     if (req.url === "/"){
 
-        //res.write("Content Here!");
-        fs.readFile('index.php', (error, data) => {
+        //Serve index if someone comes to the website root
+
+        fs.readFile('index.html', (error, data) => {
 
             if (error){
-
+                console.log(error);
                 res.writeHead(500);
-                res.write(error);
                 res.end();
 
             }
@@ -25,11 +32,49 @@ let server = http.createServer( (req,res) => {
 
         });
 
-        res.end();
+    }
+    else if (req.url === "/save") {
+
+        //Handle the request to save a family.
+        //This is mostly currently a placeholder
+        if (req.method === "POST"){
+
+
+            let body = "";
+
+            req.on("data", (chunk) => {
+                //Get chunks and append them to the body as they come in
+                body += chunk;
+
+            });
+
+            req.on("end", () => {
+
+                let parsed = JSON.parse(body);
+                console.log(parsed); //just proving that I'm getting a family
+                res.writeHead(200, { "Content-Type":"text/html" });
+                res.write("<html>Family Successfully Saved</html>");
+                res.end();
+
+                //TODO: Save to the database of your choice
+
+
+            });
+
+
+        }
+        else {
+            //Bad Request if save is not post request
+            res.writeHead(400);
+            res.write("<h2>404</h2>");
+            res.end();
+        }
+
 
     }
     else {
-
+        //Mandatory 404
+        res.writeHead(404);
         res.write("<h2>404</h2>");
         res.end();
 
@@ -38,6 +83,6 @@ let server = http.createServer( (req,res) => {
 
 });
 
-server.listen(80);
+server.listen(80);  //Listening on super-safe port 80!
 
 console.log("Running on port 80");
